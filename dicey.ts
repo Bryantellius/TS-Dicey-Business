99fc844292c8fc7b442d750a01187fdded4c9fe5
Ben Bryant <brbryant2639@gmail.com>
2020-03-31 11:05:57 -0500

    completed objectives, added delete/update methods to die, with corresponding btns in index.html

--- dicey.ts
+++ dicey.ts
@@ -1,27 +1,70 @@
 import $ from "jquery";
+let date = {
+  month: new Date().getUTCMonth() + 1,
+  day: new Date().getUTCDate(),
+  year: new Date().getUTCFullYear()
+};
+let count = 0;
+let dieArr: Array<Die> = [];
 
 class Die {
+  id: number;
   value: number;
-  constructor() {
+  dieBody: JQuery<HTMLElement>;
+  constructor(id: number) {
+    this.id = id;
     this.value = 0;
+    this.dieBody = $(`<div></div>`);
   }
 
   roll() {
     this.value = Math.floor(Math.random() * 6) + 1;
-    return this.value;
   }
 
   create() {
-    $("#diceTable").append(`
-        <div>
-            <span>${this.value}</span>
-        </div>
-    `);
+    this.dieBody = $(`
+    <div id="${this.id}" class="die bg-dark text-light rounded shadow m-2 text-center">
+        ${this.value}
+    </div>`);
+    $(this.dieBody).click(() => {
+      this.roll();
+      this.update();
+    });
+    $(this.dieBody).dblclick(() => this.delete());
+    $("#diceTable").append(this.dieBody);
+  }
+
+  update() {
+    $(this.dieBody).text(this.value);
+  }
+
+  delete() {
+    $(`#${this.id}`).remove();
+    dieArr = dieArr.filter(die => !(die.id === this.id));
   }
 }
 
-$("#btnBar").click(() => {
-  let die = new Die();
+$("#date").text(`${date.month}/${date.day}/${date.year}`);
+
+$("#addBtn").click(() => {
+  let die = new Die(count);
   die.roll();
   die.create();
+  dieArr.push(die);
+  count++;
+});
+
+$("#rollBtn").click(() => {
+  dieArr.forEach(die => {
+    die.roll();
+    die.update();
+  });
+});
+
+$("#sumBtn").click(() => {
+  let sum: number = 0;
+  dieArr.map(die => {
+    sum += die.value;
+  });
+  $("#sum").text(`Sum: ${sum}`);
 });
